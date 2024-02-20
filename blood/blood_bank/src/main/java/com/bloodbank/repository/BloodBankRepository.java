fbbdd63941e97538e9fe18793fdba9f8c924b7b2
EdigaJyoti <152795576+EdigaJyoti@users.noreply.github.com>
2024-02-20 21:58:10 +0530

    Delete blood directory

--- blood/blood_bank/src/main/java/com/bloodbank/repository/BloodBankRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bloodbank.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bloodbank.entity.Blood_Bank;
-
-public interface BloodBankRepository extends JpaRepository<Blood_Bank, Long> {
-
-}
